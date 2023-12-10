bb65f68c7c831271ba3b4274135803f61dcde9eb
Vincent_tt <evgeniy161102@gmail.com>
2023-12-10 14:34:26 +0300

    fux

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from 'fs'
 import cors from "cors";
 const app = express();
 app.use(express.json());
@@ -32,6 +33,9 @@ mongoose
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if(!fs.existsSync("uploads")){
+      fs.mkdirSync('uploads')
+    }
     cb(null, "uploads");
   },
   filename: (_, file, cb) => {
